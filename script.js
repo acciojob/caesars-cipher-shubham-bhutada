cb3bf90f788e2691b33b25c1dafe60b9f17c1db8
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-08-21 10:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Your Script here.
 
+
 const lookup = {
   A: "N",
   B: "O",
@@ -34,13 +35,17 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	for (let i = 0; i < encodedStr.length; i++) {
+		let character = encodedStr.charAt[i];
+		decodedArr[i] = lookup.character;
+	}
 
-  return; //return decodedArr
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO")); 
 
 // Do not change this line
 window.rot13 = rot13;
